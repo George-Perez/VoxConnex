9e612e4962f04010b0c51a0961538c0fc649fb6a
Campbell20 <jcampbell145@alamo.edu>
2018-02-28 13:40:40 -0600

    Added Data Annotations

--- VoxConnex/Models/QuoteQuestionnaire.cs
+++ VoxConnex/Models/QuoteQuestionnaire.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,21 +10,69 @@ namespace VoxConnex.Models
     {
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Field can't be empty")]
+        [Display(Name = "User Amount")]
         public int HowManyUsers { get; set; }
+
+        [Required(ErrorMessage = "Field can't be empty")]
+        [Display(Name = "New Phone?")]
         public bool NeedPhones { get; set; }
-        public bool NeedNumber { get; set;}
+
+
+        [Required(ErrorMessage = "Field can't be empty")]
+        [Display(Name = "New Number?")]
+        public bool NeedNumber { get; set; }
+
+        //User will be sent to this question if NeedNumber is false
+        [Display(Name = "Number Porting")]
         public int HowManyNumbersPorting { get; set; }
+
+        [Display(Name = "Location Amount")]
         public int HowManyLocations { get; set; }
+
+        [Required(ErrorMessage = "Field can't be empty")]
+        [Display(Name = "Company Name")]
+        [RegularExpression("/^[.@&]?[a - zA - Z0 - 9] +[!.@&()]?[a - zA - Z0 - 9!()] +/", ErrorMessage = "You must type in a valid company name.")]
         public string CompanyName { get; set; }
+
+        [Required(ErrorMessage = "Field can't be empty")]
+        [Display(Name = "Contact Name")]
+        [RegularExpression("/^[a-z ,.'-]+$/i", ErrorMessage = "Invalid name format.")]
         public string ContactName { get; set; }
+
+        [Required(ErrorMessage = "Field can't be empty")]
+        [Display(Name = "Street")]
         public string Street1 { get; set; }
+
+        [Display(Name = "Apt")]
         public string Street2 { get; set; }
+
+        [Required(ErrorMessage = "Field can't be empty")]
+        [Display(Name = "City")]
         public string CityName { get; set; }
+
+        [Display(Name = "State")]
         public string State { get; set; }
+
+        [Required(ErrorMessage = "Field can't be empty")]
+        [Display(Name = "Zipcode")]
         public int Zipcode { get; set; }
+
+
+        [Required(ErrorMessage = "Field can't be empty")]
+        [Display(Name = "Phone Number")]
         public string PhoneNumber { get; set; }
+
+
+
+        [Required(ErrorMessage = "Field can't be empty")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
         public string Email { get; set; }
+
+
+
+        [Required(ErrorMessage = "Field can't be empty")]
         public bool AcceptTerms { get; set; }
+
     }
 }
