9d438661cc5704c75d0280b7ec6b6369da111baa
Campbell20 <jcampbell145@alamo.edu>
2018-02-28 14:03:57 -0600

    Added Antotations

--- VoxConnex/Controllers/QuoteQuestionnairesController.cs
+++ VoxConnex/Controllers/QuoteQuestionnairesController.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.Entity;
-using System.Linq;
+﻿using System.Linq;
 using System.Net;
-using System.Web;
 using System.Web.Mvc;
 using VoxConnex.Models;
 using VoxConnex.Data;
+using System.Data.Entity;
 
 namespace VoxConnex.Controllers
 {
